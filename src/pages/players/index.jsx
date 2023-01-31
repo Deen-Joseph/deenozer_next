c9e65ebca704f7c1af7932f5ca03c77df273b928
joseph-deen <110327153+joseph-deen@users.noreply.github.com>
2023-01-31 16:10:21 +0530

    filter props

--- src/pages/players/index.jsx
+++ src/pages/players/index.jsx
@@ -6,115 +6,123 @@ import Image from "next/image";
 
 const Players = () => {
   const [data, setData] = useState("");
-  const getAllData = () => {
+   const [bata, setBata] = useState([]);
+
+ 
+let url = ""
+
+
+  const getAllData = (id) => {
+    if(id){
+       url =`http://localhost:3001/players/${id}`
+    }else url = `http://localhost:3001/players`
     axios
-      .get("http://localhost:3001/players")
+      .get(url)
       .then((response) => {
-        console.log(response.data);
-        setData(response.data[7]);
+        if(!id) {setData(response.data[0]) }else 
+        setData(response.data)
+        
+         setBata(response.data)
+         console.log(url)
       })
       .catch((error) => {
         console.log(error);
       });
   };
+
+ 
   useEffect(() => {
     getAllData();
   }, []);
 
   return (
-
-
-
-
     <div>
-    <div class="m-12">
-      <div class="flex flex-wrap gap-10">
-        <div
-          className={`w-1/6 soup flex-wrap rounded-lg shadow-lg ${style.colorCont} `}
-        >
-          <div className={`${style.searchFont} text-2xl flex items-center justify-between leading-tight p-2 md:p-4`}>
-            <p >Search Players</p>
-          </div>
-          <SearchBar></SearchBar>
-        </div>
-        <div
-          class={`w-4/5 flex flex-wrap  ${style.soup} ${style.aboutContainer}`}
-        >
+      <div className="m-12">
+        <div className="flex flex-wrap gap-10">
           <div
-            class="bg-cover bg-bottom  w-full md:w-1/4 h-70"
-            style={{
-              backgroundImage:
-                " url('https://media.bleacherreport.com/w_800,h_533,c_fill/br-img-images/002/411/828/ScreenShot2013-07-23at9.25.57AM_crop_north.jpg')",
-            }}
+            className={`w-1/6 soup flex-wrap rounded-lg shadow-lg ${style.colorCont} `}
           >
-            <div class="absolute text-xl">
-              <i class="fa fa-heart text-white hover:text-red-light ml-4 mt-4 cursor-pointer"></i>
+            <div
+              className={`${style.searchFont} text-2xl flex items-center justify-between leading-tight p-2 md:p-4`}
+            >
+              <p>Search Players</p>
             </div>
+            <SearchBar bata={bata}></SearchBar> 
           </div>
+          <div
+            className={`w-4/5 flex flex-wrap  ${style.soup} ${style.aboutContainer}`}
+          >
+            <div
+              className="bg-cover bg-bottom  w-full md:w-1/4 h-70"
+              style={{
+                backgroundImage:
+                  " url('https://media.bleacherreport.com/w_800,h_533,c_fill/br-img-images/002/411/828/ScreenShot2013-07-23at9.25.57AM_crop_north.jpg')",
+              }}
+            >
+              <div className="absolute text-xl">
+                <i className="fa fa-heart text-white hover:text-red-light ml-4 mt-4 cursor-pointer"></i>
+              </div>
+            </div>
 
-          <div class={`w-full md:w-2/3 ${style.aboutContainer}`}>
-            <div class="h-full mx-auto px-6 md:px-0 md:pt-6 md:pb-6 md:-ml-6 relative">
-              <div
-                class={`bg-white lg:h-full p-6 -mt-6 md:mt-0 relative mb-4 md:mb-0 flex flex-wrap md:flex-wrap items-center ${style.about}`}
-              >
-                <div class="w-full lg:w-1/4 lg:border-right lg:border-solid text-center md:text-left ml-6 mb-2 mr-6 pr-2">
-                  <table>
-                    <tbody>
-                      <tr>
-                        <th>Full Name</th>
-                        <td>:</td>
-                        <td>{data.first_name} {data.last_name}</td>
-                      </tr>
-                      <tr>
-                        <th>Player ID</th>
-                        <td>:</td>
-                        <td>320000</td>
-                      </tr>
-                      <tr>
-                      <th>Position</th>
-                      <td>:</td>
-                      <td>{data.position}</td>
-                    </tr>
-                    <tr>
-                    <th>Club</th>
-                    <td>:</td>
-                    <td>{data.club}</td>
-                  </tr>
-                      <tr>
-                        <th>Nationality</th>
-                        <td>:</td>
-                        <td>{data.nationality}</td>
-                      </tr>
-                      <tr>
-                        <th>Gender</th>
-                        <td>:</td>
-                        <td>{data.gender}</td>
-                      </tr>
-                      <tr>
-                        <th>Current Club</th>
-                        <td>:</td>
-                        <td>{data.club}</td>
-                      </tr>
-                      <tr>
-                        <th>Ranking</th>
-                        <td>:</td>
-                        <td>5</td>
-                      </tr>
-                      <tr>
-                        <th>Date of Birth</th>
-                        <td >:</td>
-                        <td> {data.dob}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+            <div className={`w-full md:w-2/3 ${style.aboutContainer}`}>
+              <div className="h-full mx-auto px-6 md:px-0 md:pt-6 md:pb-6 md:-ml-6 relative">
+                <div
+                  className={`bg-white lg:h-full p-6 -mt-6 md:mt-0 relative mb-4 md:mb-0 flex flex-wrap md:flex-wrap items-center ${style.about}`}
+                >
+                  <div className="w-full lg:w-1/4 lg:border-right lg:border-solid text-center md:text-left ml-6 mb-2 mr-6 pr-2">
+                    <table>
+                      <tbody>
+                        <tr>
+                          <th>Full Name</th>
+                          <td>:</td>
+                          <td className="pl-2">
+                            {data.first_name} {data.last_name}
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>Player ID</th>
+                          <td>:</td>
+                          <td className="pl-2">320000</td>
+                        </tr>
+                        <tr>
+                          <th>Position</th>
+                          <td>:</td>
+                          <td className="pl-2">{data.position}</td>
+                        </tr>
 
-                <div class="w-full lg:w-3/5 pl-4">
-                  <p class="text-lg font-bold mb-2">About</p>
-                  <p class="text-md  text-justify text-md">
-                  {data.about}
-                  </p>
+                        <tr>
+                          <th>Nationality</th>
+                          <td>:</td>
+                          <td className="pl-2">{data.nationality}</td>
+                        </tr>
+                        <tr>
+                          <th>Gender</th>
+                          <td>:</td>
+                          <td className="pl-2">{data.gender}</td>
+                        </tr>
+                        <tr>
+                          <th>Current Club</th>
+                          <td>:</td>
+                          <td className="pl-2">{data.club}</td>
+                        </tr>
+                        <tr>
+                          <th>Ranking</th>
+                          <td>:</td>
+                          <td className="pl-2">{data.ranking}</td>
+                        </tr>
+                        <tr>
+                          <th>Date of Birth</th>
+                          <td>:</td>
+                          <td className="pl-2"> {data.dob?.split("T")[0]}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+
+                  <div className="w-full lg:w-3/5 pl-4">
+                    <p className="text-lg font-bold mb-2">About</p>
+                    <p className="text-md  text-justify text-md">{data.about}</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -122,7 +130,6 @@ const Players = () => {
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
